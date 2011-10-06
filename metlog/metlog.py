e73added0039282e30ea5d2edf0680ff766f0ee3
Rob Miller <rob@kalistra.com>
2011-10-06 15:57:23 -0700

    initial commit

---
+++ metlog/metlog.py
@@ -0,0 +1,84 @@
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1/GPL 2.0/LGPL 2.1
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# The Original Code is metlog
+#
+# The Initial Developer of the Original Code is the Mozilla Foundation.
+# Portions created by the Initial Developer are Copyright (C) 2011
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s):
+#   Rob Miller (rmiller@mozilla.com)
+#
+# Alternatively, the contents of this file may be used under the terms of
+# either the GNU General Public License Version 2 or later (the "GPL"), or
+# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+# in which case the provisions of the GPL or the LGPL are applicable instead
+# of those above. If you wish to allow use of your version of this file only
+# under the terms of either the GPL or the LGPL, and not to allow others to
+# use your version of this file under the terms of the MPL, indicate your
+# decision by deleting the provisions above and replace them with the notice
+# and other provisions required by the GPL or the LGPL. If you do not delete
+# the provisions above, a recipient may use your version of this file under
+# the terms of any one of the MPL, the GPL or the LGPL.
+#
+# ***** END LICENSE BLOCK *****
+try:
+    import simplejson as json
+except ImportError:
+    import json
+
+from datetime import datetime
+from socket import socket, AF_INET, SOCK_DGRAM
+
+
+class MetlogClient(object):
+    """
+    Client class encapsulating metlog API, and providing storage for default
+    values for various metlog call settings.
+    """
+    def __init__(self, host, port, logger='', severity=6):
+        self.host = host
+        self.port = port
+        self.logger = logger
+        self.severity = severity
+        self.flavors = dict()
+        self.udpsock = socket(AF_INET, SOCK_DGRAM)
+
+    def set_message_flavor(self, flavor_name, metadata):
+        self.flavors[flavor_name] = metadata
+
+    def _send_message(self, full_msg):
+        json_msg = json.dumps(full_msg)
+        self.udpsock.sendto(json_msg, (self.host, self.port))
+
+    def metlog(self, timestamp=None, logger=None, severity=None, message='',
+               metadata=None, flavors=None):
+        timestamp = timestamp if timestamp is not None else datetime.utcnow()
+        logger = logger if logger is not None else self.logger
+        severity = severity if severity is not None else self.severity
+        metadata = metadata if metadata is not None else dict()
+        if flavors:
+            for flavor in flavors:
+                metadata.update(self.flavors.get(flavor, dict()))
+        full_msg = dict(timestamp=timestamp, logger=logger, severity=severity,
+                        message=message, metadata=metadata)
+        self._send_msg(full_msg)
+
+    def timer(self, name, timestamp=None, logger=None, severity=None,
+              metadata=None, flavors=None, rate=1):
+        pass
+
+    def incr(self, name, timestamp=None, logger=None, severity=None,
+             metadata=None, flavors=None):
+        pass
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='metlog',
+      version=version,
+      description="Metrics Logging",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Rob Miller',
+      author_email='rmiller@mozilla.com',
+      url='',
+      license='MPLv1.1',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
