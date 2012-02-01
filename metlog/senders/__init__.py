f8ccf5741b0e385dce2587b9ed1cbaf8f0b79913
Rob Miller <rob@kalistra.com>
2012-02-01 11:52:34 -0800

    switch to MPL2.0 include text, and separate the senders out into separate modules in a package instead of a single module

---
+++ metlog/senders/__init__.py
@@ -0,0 +1,16 @@
+# ***** BEGIN LICENSE BLOCK *****
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# The Initial Developer of the Original Code is the Mozilla Foundation.
+# Portions created by the Initial Developer are Copyright (C) 2012
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s):
+#   Rob Miller (rmiller@mozilla.com)
+#
+# ***** END LICENSE BLOCK *****
+from metlog.senders.zmq import ZmqPubSender  # NOQA
+from metlog.senders.dev import StdOutSender  # NOQA
+from metlog.senders.dev import DebugCaptureSender  # NOQA
--- metlog/senders/zmq.py
+++ metlog/senders/zmq.py
@@ -1,48 +1,26 @@
 # ***** BEGIN LICENSE BLOCK *****
-# Version: MPL 1.1/GPL 2.0/LGPL 2.1
-#
-# The contents of this file are subject to the Mozilla Public License Version
-# 1.1 (the "License"); you may not use this file except in compliance with
-# the License. You may obtain a copy of the License at
-# http://www.mozilla.org/MPL/
-#
-# Software distributed under the License is distributed on an "AS IS" basis,
-# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
-# for the specific language governing rights and limitations under the
-# License.
-#
-# The Original Code is metlog
-#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
 # The Initial Developer of the Original Code is the Mozilla Foundation.
-# Portions created by the Initial Developer are Copyright (C) 2011
+# Portions created by the Initial Developer are Copyright (C) 2012
 # the Initial Developer. All Rights Reserved.
 #
 # Contributor(s):
 #   Rob Miller (rmiller@mozilla.com)
 #
-# Alternatively, the contents of this file may be used under the terms of
-# either the GNU General Public License Version 2 or later (the "GPL"), or
-# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
-# in which case the provisions of the GPL or the LGPL are applicable instead
-# of those above. If you wish to allow use of your version of this file only
-# under the terms of either the GPL or the LGPL, and not to allow others to
-# use your version of this file under the terms of the MPL, indicate your
-# decision by deleting the provisions above and replace them with the notice
-# and other provisions required by the GPL or the LGPL. If you do not delete
-# the provisions above, a recipient may use your version of this file under
-# the terms of any one of the MPL, the GPL or the LGPL.
-#
 # ***** END LICENSE BLOCK *****
+from __future__ import absolute_import
 try:
     import simplesjson as json
 except ImportError:
-    import json
-import sys
+    import json  # NOQA
 import threading
 try:
     import zmq
 except ImportError:
-    zmq = None
+    zmq = None  # NOQA
 
 
 # We need to set the maximum number of outbound messages so that
@@ -98,30 +76,3 @@ class ZmqPubSender(object):
         """
         json_msg = json.dumps(msg)
         self.publisher.send(json_msg)
-
-
-class StdOutSender(object):
-    """
-    Emits metlog messages to stdout for dev purposes.
-    """
-    def send_message(self, msg):
-        json_msg = json.dumps(msg)
-        sys.stdout.write('%s\n' % json_msg)
-        sys.stdout.flush()
-
-
-class DebugCaptureSender(object):
-    """
-    Capture upto 100 metlog messages in a circular buffer for
-    inspection later
-
-    This is only for DEBUGGING.  Do not use this for anything except
-    development.
-    """
-    def __init__(self):
-        import collections
-        self.msgs = collections.deque(maxlen=100)
-
-    def send_message(self, msg):
-        json_msg = json.dumps(msg)
-        self.msgs.append(json_msg)
