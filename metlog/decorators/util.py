bfe3b2fdee832d3b45695eacec519822dea87973
Rob Miller <rob@kalistra.com>
2012-02-28 17:02:33 -0800

    remove some no longer valid tests

--- metlog/decorators/util.py
+++ metlog/decorators/util.py
@@ -1,11 +1,12 @@
 # ***** BEGIN LICENSE BLOCK *****
 # This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file,
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Contributor(s):
+#   Victor Ng (vng@mozilla.com)
+#
 # ***** END LICENSE BLOCK *****
-
-from metlog.helper import HELPER
 
 
 def return_fq_name(func, klass=None):
