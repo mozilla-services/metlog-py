64bd663cd6af0dd6605946cef207dcc01cfbe093
Victor Ng <crankycoder@gmail.com>
2012-04-16 12:52:08 -0400

    retrofitted zmq sender module to use the new zmq connection pool

--- metlog/senders/zmq.py
+++ metlog/senders/zmq.py
@@ -22,12 +22,121 @@ try:
 except ImportError:
     zmq = None  # NOQA
 
+import Queue
+import sys
 
 # We need to set the maximum number of outbound messages so that
 # applications don't consume infinite memory if outbound messages are
 # not processed
 MAX_MESSAGES = 1000
 
+
+class HandshakingClient(object):
+    def __init__(self, context, handshake_bind, connect_bind,
+            handshake_timeout=200, 
+            hwm=200):
+
+        self.context = context
+
+        self.handshake_bind = handshake_bind
+        self.connect_bind = connect_bind
+
+        self.handshake_timeout = handshake_timeout
+        self.hwm = hwm
+
+        self.handshake_socket = None
+        self.socket = None
+
+        self._connected = False
+
+    def connect(self):
+        # Socket to send handshake signals
+        self.handshake_socket = self.context.socket(zmq.REQ)
+        self.handshake_socket.connect(self.handshake_bind)
+        self.handshake_socket.setsockopt(zmq.LINGER, 0)
+
+        # Socket to actually doi pub/sub
+        self.socket = self.context.socket(zmq.PUB)
+        self.socket.connect(self.connect_bind)
+        self.socket.setsockopt(zmq.LINGER, 0)
+        self.socket.setsockopt(zmq.HWM, self.hwm)
+
+        poll = zmq.Poller()
+        poll.register(self.handshake_socket, zmq.POLLIN)
+
+        self.handshake_socket.send("")
+        socks = dict(poll.poll(self.handshake_timeout))
+
+        if socks.get(self.handshake_socket) == zmq.POLLIN:
+            self.handshake_socket.recv()
+            self.handshake_socket.close()
+            self._connected = True
+            return True
+        else:
+            self._connected = False
+            self.close()
+            return False
+
+    def send(self, msg):
+        try:
+            if self._connected:
+                self.socket.send(msg)
+            elif self.connect():
+                self.socket.send(msg)
+            else:
+                sys.stderr.write(msg)
+                sys.stderr.flush()
+        except zmq.ZMQError:
+            self.close()
+            sys.stderr.write(msg)
+            sys.stderr.flush()
+
+    def close(self):
+        try:
+            self.handshake_socket.close()
+        except ZQError, err:
+            pass
+
+        try:
+            self.socket.close()
+        except ZMQError, err:
+            pass
+
+def client_factory(context):
+    def get_client():
+        return Client(context, 'tcp://localhost:5562', 'tcp://localhost:5561')
+    return get_client
+
+class Pool(object):
+    """
+    This is a threadsafe poool of 0mq clients.
+
+    :param client_factory:
+        a factory function which takes no arguments and will generate
+        client instances. Clients are responsible to rebind themselves
+        if necessary
+    :param size: The number of clients to create in the pool
+    """
+
+    def __init__(self, client_factory, size=10):
+        self._clients = Queue.Queue()
+        for i in range(size):
+            self._clients.put(client_factory())
+
+    def send(self, msg):
+        sock = None
+        try:
+            sock = self.socket()
+            sock.send(msg)
+        except Queue.Empty:
+            # Sometimes, we'll get nothing
+            sys.stderr.write(msg)
+        finally:
+            if sock:
+                self._clients.put(sock)
+
+    def socket(self):
+        return self._clients.get()
 
 class ZmqPubSender(object):
     """
@@ -42,33 +151,20 @@ class ZmqPubSender(object):
             raise ValueError('Must have `pyzmq` installed to use ZmqPubSender')
         return super(ZmqPubSender, cls).__new__(cls)
 
-    def __init__(self, bindstrs, queue_length=MAX_MESSAGES):
-        if isinstance(bindstrs, basestring):
-            bindstrs = [bindstrs]
-        self.bindstrs = bindstrs
-        self._queue_length = queue_length
+    def __init__(self, handshake_bind, 
+                    connect_bind,
+                    handshake_timeout,
+                    pool_size=10,
+                    queue_length=MAX_MESSAGES):
 
-        # The threadlocal is on the *instance* instead of the class so
-        # that we can create multiple instances of the ZmqPubSender
-        # that target dfferent bindstrings
-        self._local = threading.local()
+        def get_client():
+            return HandshakingClient(_zmq_context, 
+                    handshake_bind, 
+                    connect_bind, 
+                    handshake_timeout,
+                    queue_length)
 
-    @property
-    def publisher(self):
-        """
-        Property that exposes zmq publisher socket. Implemented as a property
-        because zmq sockets are not threadsafe and thus the actual socket
-        object is stored as a threadlocal value.
-        """
-        if not hasattr(self._local, 'publisher'):
-            # 0mq sockets aren't threadsafe, so bind them into a
-            # threadlocal
-            self._local.publisher = self._zmq_context.socket(zmq.PUB)
-            self._local.publisher.setsockopt(zmq.HWM, self._queue_length)
-
-            for bindstr in self.bindstrs:
-                self._local.publisher.connect(bindstr)
-        return self._local.publisher
+        self.pool = Pool(get_client, pool_size)
 
     def send_message(self, msg):
         """
@@ -77,4 +173,4 @@ class ZmqPubSender(object):
         :param msg: Dictionary representing the message.
         """
         json_msg = json.dumps(msg)
-        self.publisher.send(json_msg)
+        self.pool.send(json_msg)
