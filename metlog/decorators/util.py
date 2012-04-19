0bd9056b0f4ab07e1ff93c27a5e570ce89f0ad24
Victor Ng <crankycoder@gmail.com>
2012-04-19 16:38:27 -0400

    changed the function name resolver to use dotted names for all cases instead of colons.  This is to satisfy the statsd protocol

--- metlog/decorators/util.py
+++ metlog/decorators/util.py
@@ -21,7 +21,7 @@ def return_fq_name(func, klass=None):
     func_module = getattr(func, '__module__', None)
 
     if klass:
-        name = "%s:%s.%s" % (klass.__module__, \
+        name = "%s.%s.%s" % (klass.__module__, \
                              klass.__name__, \
                              'PyCallable')
     elif func_name:
@@ -31,7 +31,7 @@ def return_fq_name(func, klass=None):
         # and that means the function is not yet bound to an object
         # instance yet
         # Just grab the containing module and the function name
-        name = "%s:%s" % (func_module, func_name)
+        name = "%s.%s" % (func_module, func_name)
     else:
         # This shouldn't happen, but we don't really want to throw
         # errors just because we can't get some fake arbitrary
