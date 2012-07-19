96d7ff0d3c24d3ea3ecd40968da65fde4a1d2dd0
Rob Miller <rob@kalistra.com>
2012-07-18 17:17:15 -0700

    version bump 0.9.3 -> 0.9.4

--- MetlogPy.spec
+++ MetlogPy.spec
@@ -1,6 +1,6 @@
 %define name metlog-py
 %define pythonname MetlogPy
-%define version 0.9.3
+%define version 0.9.4
 %define unmangled_version %{version}
 %define release 0
 
--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,7 @@ copyright = u'2012, Mozilla Services'
 # built documents.
 #
 # The short X.Y version.
-version = '0.9.3'
+version = '0.9.4'
 # The full version, including alpha/beta/rc tags.
 release = version
 
