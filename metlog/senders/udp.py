eaf532632813fe158b71bbcb6b61f7a4dbd65bb7
Rob Miller <rob@kalistra.com>
2012-09-10 15:27:25 -0700

    add support for multiple UDP listeners w/ a single sender

--- metlog/senders/udp.py
+++ metlog/senders/udp.py
@@ -13,6 +13,7 @@
 # ***** END LICENSE BLOCK *****
 
 from __future__ import absolute_import
+from types import StringTypes
 
 try:
     import simplejson as json
@@ -28,15 +29,34 @@ class UdpSender(object):
     """
 
     def __init__(self, host, port):
-        self._host = host
-        self._port = port
+        """
+        Create UdpSender object.
+
+        :param host: A string or sequence of strings representing the hosts to
+                     which messages should be delivered.
+        :param port: An integer or sequence of integers representing the ports
+                     to which the messages should be delivered. Will be zipped
+                     w/ the provided hosts to generate host/port pairs. If
+                     there are extra hosts, the last port in the sequence will
+                     be repeated for each extra host. If there are extra ports
+                     they will be truncated and ignored.
+        """
+        if isinstance(host, StringTypes):
+            host = [host]
+        if isinstance(port, int):
+            port = [port]
+        num_extra_hosts = len(host) - len(port)
+        if num_extra_hosts > 0:
+            port.extend(num_extra_hosts * [port[-1]])
+        self._destinations = zip(host, port)
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     def send_message(self, msg):
         """
-        Serialize and send a message off to the metlog listener.
+        Serialize and send a message off to the metlog listener(s).
 
         :param msg: Dictionary representing the message.
         """
         json_msg = json.dumps(msg)
-        self.socket.sendto(json_msg, (self._host, self._port))
+        for host, port in self._destinations:
+            self.socket.sendto(json_msg, (host, port))
