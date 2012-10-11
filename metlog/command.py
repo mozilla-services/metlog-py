7c02a58219c7d7e63e2655c482bf05bd0d5eaa42
Rob Miller <rob@kalistra.com>
2012-10-11 13:04:19 -0700

    add `--raw` flag for mb tool to do less work sending each message

--- metlog/command.py
+++ metlog/command.py
@@ -12,13 +12,16 @@
 #   Rob Miller (rmiller@mozilla.com)
 #
 # ***** END LICENSE BLOCK *****
+from datetime import datetime
 from docopt import docopt
 from metlog.config import client_from_dict_config, client_from_stream_config
+import json
+import socket
 
 mb_doc = """mb: MetlogBench, blast messages at a Metlog router.
 
 Usage:
-  mb HOST PORT [--metlogcfg=<ini file>]
+  mb HOST PORT [--metlogcfg=<ini_file>] [--raw]
 
 Arguments:
   HOST             Hostname or IP address of metlog router to test
@@ -26,6 +29,8 @@ Arguments:
 
 Options:
   --metlogcfg=<ini file>        Path to metlog client config file
+  --raw                         Raw send of static text directly over UDP
+                                instead of using metlog client
 """
 
 
@@ -39,6 +44,17 @@ def mb():
                                  'args': (host, port),
                                  },
                       }
+
+    if arguments.get('--raw'):
+        udpsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        msg = {"severity": 6, "timestamp": datetime.now().isoformat(),
+               "metlog_hostname": "spire",
+               "fields": {"userid": 25, "req_time": 4}, "metlog_pid": 34328,
+               "logger": "syncstorage", "type": "services", "payload": "foo",
+               "env_version": "0.8"}
+        json_msg = json.dumps(msg)
+        while True:
+            udpsock.sendto(json_msg, (host, port))
 
     if arguments.get('--metlogcfg'):
         with open(arguments['--metlogcfg']) as cfgfile:
